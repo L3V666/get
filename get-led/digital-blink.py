3c8af38cb2e4393fcbf23e77352cedae1fa562c0
Lew L3V666 Podmoskovnov <lev.tmb.l29@gmail.com>
2025-09-16 15:13:40 +0300

    add jumping-led

--- get-led/digital-blink.py
+++ get-led/digital-blink.py
@@ -8,9 +8,9 @@ led = 26
 GPIO.setup(led, GPIO.OUT)
 
 state = 0
-period = 1.0
+half_period = 1.0
 
 while True:
     GPIO.output(led, state)
     state = not state
-    time.sleep(period)
+    time.sleep(half_period)
