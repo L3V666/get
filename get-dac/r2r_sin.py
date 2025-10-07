99661a75692358c3c77146854cab7d522a0f281c
Lew L3V POdmoskovnov <lev.tmb.l29@gmail.com>
2025-10-07 16:58:01 +0300

    07.10.2025

--- get-dac/r2r_sin.py
+++ get-dac/r2r_sin.py
@@ -3,14 +3,19 @@ import signal_generator as sg
 import time
 
 amplitude = 3.2
-signal_frequence = 10
-sampling_frequence = 1000
+signal_frequency = 10
+sampling_frequency = 1000
 
 try:
-    dac = r2r.R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.15)
+    dac = r2r.R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.2)
+    start_time = time.time()
     
     while True:
-        dac.set_voltage(sg.get_sin_wave_amplitude(amplitude, time))
+        current_time = time.time() - start_time
+        signal_value = sg.get_sin_wave_amplitude(signal_frequency, current_time)
+        voltage = amplitude * signal_value
+        dac.set_voltage(voltage)
+        sg.wait_for_sampling_period(sampling_frequency)
     
 finally:
     dac.deinit()
--- get-dac/signal_generator.py
+++ get-dac/signal_generator.py
@@ -1,9 +1,12 @@
 import numpy as np
 import time
 
-def get_sin_wave_amplitude(freq, time):
-    return ((np.sin(2 * np.pi * freq * time) + 1) / 2)
+def get_sin_wave_amplitude(freq, t):
+    sin_value = np.sin(2 * np.pi * freq * t)
+    shifted = sin_value + 1
+    normalized = shifted / 2
+    return normalized
 
 def wait_for_sampling_period(sampling_frequency):
-    sampling_period = 1.0 / sampling_frequency
-    time.sleep(sampling_period)
+    period = 1.0 / sampling_frequency
+    time.sleep(period + 1)
